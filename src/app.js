b9a2a1bb9879582916a0274d3030b444e720906b
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2023-05-07 16:39:32 -0300

    feat/ add handlebars config

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
+const { engine } = require('express-handlebars')
 const express = require('express')
 const apiRouter = require('./routes/api.router.js')
 const ErrorHandler = require('./middlewares/ErrorHandler.js')
-
 const app = express()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/', express.static('public'))
 
 app.use('/api', apiRouter)
 app.use(ErrorHandler.intercept)
 
 const PORT = 8080
+
 app.listen(PORT, () => {
   console.log(`Server up and running on port ${PORT} 🚀`)
 })
